a8a44e48bb9084c9323007739164a085d24906db
fnasgod72 <kangarootoon@gmail.com>
2025-06-20 13:45:06 +0100

    FUCK YOU
    pooooop

---
+++ EVIL_SCRIPT.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+func _on_pressed() -> void:
+	$"../AnimatedSprite2D".play("EVILMODE")
+	$"../AnimatedSprite2D".show()
+	$".".hide()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	$"../AnimatedSprite2D".hide()
+	$".".show()
